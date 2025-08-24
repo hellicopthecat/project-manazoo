fc327cd3d40f24bfda4e6d04ab5695f5f0375eaf
Zion <zion.geek.py@gmail.com>
2025-08-24 14:16:21 +0900

    chore: add doc string

--- src/app/enclosure/EnclosureManager.java
+++ src/app/enclosure/EnclosureManager.java
@@ -1,7 +1,15 @@
+
+/**
+ * 동물원 우리를 생성, 조회 및 동물 관리 등 전반적으로 관리하는 클래스입니다.
+ */
 package app.enclosure;
 
 public class EnclosureManager {
 
+	 /**
+	  * EnclosureManager를 테스트하거나 실행하는 메인 메서드입니다.
+	  * @param args 커맨드라인 인자
+	  */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
